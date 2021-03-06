8cb59015b8238b2f858aac4e76be22010146433d
Bernard Braun <bernard.sbraun@gmail.com>
2021-03-06 15:26:57 -0300

    Commit final da aula

---
+++ js/main.js
@@ -0,0 +1,126 @@
+
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar"
+    
+}
+
+function clicou() {
+    window.open("https://google.com")
+    window.location.href = "https://www.uol.com.br"
+}
+
+function trocar() {
+    document.getElementById("trocar").innerHTML = "Olha que bacana!"
+}
+
+function voltar() {
+    document.getElementById("trocar").innerHTML = "Olha aqui que legal!"
+}
+
+function load(){
+    alert("Página carregada!")
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value)
+}
+
+/*
+function soma(n1, n2) {
+    return n1+n2
+}
+
+alert(soma(5, 10))
+*/
+/*
+function validaIdade (idade){
+    
+    var validar
+    if(idade>=18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar
+
+}
+
+var idade = prompt("Qual a sua idade?")
+console.log(validaIdade(idade))
+*/
+/*
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+alert(frase="Vai Japão!")
+alert("Opa!")
+alert(setReplace("Vai Japão", "Japão", "Brasil"))
+*/
+
+/*
+var d = new Date()
+alert(d.getDay + d.getMonth)
+*/
+
+/*var count 
+
+for (count = 0; count <=5; count++) {
+    alert(count)
+}
+*/
+
+
+
+
+
+/* var count = 0
+
+while (count <= 5) {
+    alert(count)
+    count++
+}
+*/
+
+
+/*var idade = prompt("Qual a sua idade?")
+//var idade = 18
+if (idade >= 18) {
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
+*/
+
+
+
+
+
+//var nome = "Bernard Braun"
+//var idade = 32
+//var idade2 = 15
+//alert(nome + " tem " + idade + " anos.")
+//alert(idade + idade2)
+
+//var frutas = ["Pera", "Uva", "Maçã"]
+
+//var fruta = {nome:"maçã,", cor:"vermelha"}
+
+//var frutas = [{nome:"maçã,", cor:"vermelha"}, {nome:"uva,", cor:"roxa"}];
+
+//console.log(frutas[1].nome)
+
+//console.log(nome)
+//console.log(idade)
+//console.log(idade2)
+//frutas.push("Banana")
+
+//alert(frutas)
+//frutas.pop()
+//console.log(frutas.length)
+//console.log(frutas)
+//console.log(frutas.reverse())
+//console.log(frutas.toString())
+//console.log(frutas.join(" - "))
+
+//console.log(fruta.cor)
